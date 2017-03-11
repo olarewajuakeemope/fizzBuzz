4e949d61250c36d7a29927999221de7ea1f31c8a
olarewajuakeemope <olarewajuakeemopeyemi@gmail.com>
2017-03-11 09:27:50 +0100

    fizzbuzz code working for andelab tests

---
+++ app/library.js
@@ -0,0 +1,15 @@
+'use strict'
+
+module.exports = {
+ 
+ fizzBuzz: function(num) {
+   if(num%3===0&&num%5===0)
+	   return "FizzBuzz";
+   else if(num%3===0)
+	   return "Fizz";
+   else if(num%5===0)
+	   return "Buzz";
+   else if(!(num%3===0)&&!(num%5===0))
+	   return num;
+ }
+}
